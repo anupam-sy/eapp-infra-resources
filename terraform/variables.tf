6bb84736c4e3d43438f889fa8ccf820c079f4f2b
anupam <anupamyadav0224@gmail.com>
2023-02-08 00:21:37 +0530

    comment updated.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,7 +27,7 @@ variable "resource_labels" {
 variable "terraform_service_account" {
   type        = string
   description = "Terraform service account to execute the terraform code."
-  # Make sure to give "roles/iam.serviceAccountTokenCreator" role to any identity who will trigger the terraform code.
+  # Make sure to give "roles/iam.serviceAccountTokenCreator" role to an identity (who will trigger the terraform code) on this service account for the impersonation to succeed.
 }
 
 # Variable Declaration - IaaS (VPC Network, SubNetwork, Cloud VPN, GCE)
